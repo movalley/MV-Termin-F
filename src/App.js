e7ae8666cb9a2eb9c1301e99c0d36a8c50243153
Mario <rozic.mario@hotmail.com>
2019-07-26 14:23:32 +0200

    :sparkles: Remove dot

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Mario</code> Rozic.
+          <code>Mario</code> Rozic
         </p>
       </header>
     </div>
